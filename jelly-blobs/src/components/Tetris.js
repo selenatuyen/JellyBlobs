156bd4f731bef1aa4138aa7b0fc8a913e8fe131d
selenatuyen <selenaltuyen@gmail.com>
2020-09-20 13:53:24 -0700

    Add collision handling

--- jelly-blobs/src/components/Tetris.js
+++ jelly-blobs/src/components/Tetris.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {createStage } from '../gameHelper';
+import {createStage, checkCollision } from '../gameHelper';
 
 // styled components
 import {StyledTetris, StyledTetrisWrapper} from './styles/StyledTetris';
@@ -21,17 +21,31 @@ const Tetris = () => {
     const [stage, setStage] = useStage(player, resetPlayer);
 
     const movePlayer = dir => {
-        updatePlayerPos({x: dir, y: 0});
+        if(!checkCollision(player, stage, {x: dir, y: 0})){
+            updatePlayerPos({x: dir, y: 0});
+        }
     };
 
     const startGame = () => {
         //Reset everything
         setStage(createStage());
         resetPlayer();
+        setGameOver(false);
     };
 
     const drop = () => {
-        updatePlayerPos({x : 0, y : 1, collided: false});
+        if(!checkCollision(player, stage, {x: 0, y: 1})){
+            updatePlayerPos({x : 0, y : 1, collided: false});
+        }
+        else{
+            //Game Over
+            if(player.pos.y < 1){
+                console.log("GAME OVER!!");
+                setGameOver(true);
+                setDropTime(null);
+            }
+            updatePlayerPos({x: 0, y:0, collided: true});
+        }
     };
 
     const dropPlayer = () => {
