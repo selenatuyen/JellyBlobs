3d9d49ad3124baf891341259b4d433491216c955
selenatuyen <selenaltuyen@gmail.com>
2020-09-20 14:51:51 -0700

    Add game status hook and levels, score, and rows

--- jelly-blobs/src/components/Tetris.js
+++ jelly-blobs/src/components/Tetris.js
@@ -7,6 +7,8 @@ import {StyledTetris, StyledTetrisWrapper} from './styles/StyledTetris';
 // custom hooks
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage} from '../hooks/useStage';
+import { useInterval } from '../hooks/useInterval';
+import { useGameStatus } from '../hooks/useGameStatus';
 
 //Components
 import Stage from './Stage';
@@ -18,7 +20,8 @@ const Tetris = () => {
     const [gameOver, setGameOver] = useState(false);
 
     const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-    const [stage, setStage] = useStage(player, resetPlayer);
+    const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+    const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
 
     const movePlayer = dir => {
         if(!checkCollision(player, stage, {x: dir, y: 0})){
@@ -29,11 +32,21 @@ const Tetris = () => {
     const startGame = () => {
         //Reset everything
         setStage(createStage());
+        setDropTime(1000);
         resetPlayer();
         setGameOver(false);
+        setScore(0);
+        setRows(0);
+        setLevel(0);
     };
 
     const drop = () => {
+        // Increase level when player has cleared 10 rows
+        if(rows > (level + 1) * 10){
+            setLevel(prev => prev + 1);
+            // Also increase speed
+            setDropTime(1000 / (level + 1) + 200);
+        }
         if(!checkCollision(player, stage, {x: 0, y: 1})){
             updatePlayerPos({x : 0, y : 1, collided: false});
         }
@@ -48,7 +61,16 @@ const Tetris = () => {
         }
     };
 
+    const keyUp = ({keyCode}) => {
+        if(!gameOver){
+            if(keyCode === 40){
+                setDropTime(1000 / (level + 1) + 200);
+            }
+        }
+    };
+
     const dropPlayer = () => {
+        setDropTime(null);
         drop();
     };
 
@@ -69,8 +91,12 @@ const Tetris = () => {
         }
     };
 
+    useInterval(() => {
+        drop();
+    }, dropTime);
+
     return (
-        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)} onKeyUp={keyUp}>
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
@@ -78,9 +104,9 @@ const Tetris = () => {
                         <Display gameOver={gameOver} text="Game Over" /> 
                         ) : (
                         <div>
-                            <Display text="Score"/>
-                            <Display text="Rows"/>
-                            <Display text="Level"/>
+                            <Display text={`Score: ${score}`}/>
+                            <Display text={`Rows: ${rows}`}/>
+                            <Display text={`Level: ${level}`}/>
                         </div>
                     )}
                     <StartButton callback={startGame} />
--- jelly-blobs/src/hooks/useStage.js
+++ jelly-blobs/src/hooks/useStage.js
@@ -3,8 +3,22 @@ import{ createStage} from '../gameHelper';
 
 export const useStage = (player, resetPlayer) =>{
     const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage => 
+            newStage.reduce((ack, row) => {
+                if(row.findIndex(cell => cell[0] === 0) === -1){ //findIndex returns -1 if nothing found
+                    setRowsCleared(prev => prev + 1);
+                    ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return ack;
+                }
+                ack.push(row);
+                return ack;
+        }, []);
+
         const updateStage = prevStage  => {
             // First flush the stage
             const newStage = prevStage.map(row => 
@@ -23,6 +37,7 @@ export const useStage = (player, resetPlayer) =>{
             // Then check if we collided
             if(player.collided){
                 resetPlayer();
+                return sweepRows(newStage);
             }
             return newStage;
         };
@@ -31,5 +46,5 @@ export const useStage = (player, resetPlayer) =>{
 
     }, [player, resetPlayer]);
 
-    return [stage, setStage];
+    return [stage, setStage, rowsCleared];
 }
